1b7d78d03c5dabcb43481fde05ad1cc5e899a52a
AlexGAVaz <alexandergavazin@gmail.com>
2023-03-01 21:53:21 +0300

    Add 7 day

--- OperatorSWITCH/main.cpp
+++ OperatorSWITCH/main.cpp
@@ -1,14 +1,51 @@
-//
-//  main.cpp
-//  OperatorSWITCH
-//
-//  Created by Alexander on 01.03.2023.
-//
+#include <stdio.h>
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    int day; printf ("Day? ");
+    scanf ("%i",&day);
+    
+    switch (day)
+    {
+        case 1:
+        {
+            printf("Mon\n");
+        }
+            break;
+        case 2:
+        {
+            printf("Tue\n");
+        }
+            break;
+        case 3:
+        {
+            printf("Wed\n");
+        }
+            break;
+        case 4:
+        {
+            printf("Thu\n");
+        }
+            break;
+        case 5:
+        {
+            printf("Fri\n");
+        }
+            break;
+        case 6:
+        {
+            printf("Sat\n");
+        }
+            break;
+        case 7:
+        {
+            printf("Sun\n");
+        }
+            break;                
+        default:
+        {
+            printf("Error\n");
+        }
+            break;
+    }
 }
